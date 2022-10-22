9526d9c43a3157029fb5def4d5498cb65e4dfbc4
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-10-21 23:24:45 -0500

    Built out inquirer

--- index.js
+++ index.js
@@ -26,11 +26,14 @@ function init() {
                     name: 'managerOfficeNum'
                 }
             ])
-            .then((response) => 
-                console.log('Manager', response),
+            .then((response) => {
+                console.log('Manager', response)
                 // Do I create the constructor here?
                 createTeam()
-            )
+                let managerCard = `manager${response.managerName}`;
+                console.log(managerCard);
+                // let managerCard = new Manager()                
+            })
     }
 
     function createTeam() {
@@ -45,45 +48,76 @@ function init() {
             ])
             .then((response) => {
                 console.log('Create Team', response)
-    //             if (response.build == 'Add an engineer') {
-    //                 addEngineer()
-    //             } else if (response.build == 'Add an intern') {
-    //                 addIntern()
-    //             } else {buildTeam()}
+                if (response.build == 'Add an engineer') {
+                    addEngineer()
+                } else if (response.build == 'Add an intern') {
+                    addIntern()
+                } else {console.log("End")
+                // } else {buildTeam()} 
+                }   
             })    
     }
 
-    // function addEngineer() {
-    //     inquirer
-    //         .prompt([
-    //             {
-    //                 type: 'input',
-    //                 message: "Enter the engineer's name",
-    //                 name: 'engineerName'
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Enter the engineer's ID",
-    //                 name: 'engineerId'
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Enter the engineer's email",
-    //                 name: 'engineerEmail'
-    //             },
-    //             {
-    //                 type: 'input',
-    //                 message: "Enter the engineer's gitHub username",
-    //                 name: 'engineerUsername'
-    //             }
-    //         ])
-    //         .then((response) =>
-    //             console.log('Engineer', response)
-    //             createTeam()
-    //         )
-    // }
+    function addEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: "Enter the engineer's name",
+                    name: 'engineerName'
+                },
+                {
+                    type: 'input',
+                    message: "Enter the engineer's ID",
+                    name: 'engineerId'
+                },
+                {
+                    type: 'input',
+                    message: "Enter the engineer's email",
+                    name: 'engineerEmail'
+                },
+                {
+                    type: 'input',
+                    message: "Enter the engineer's gitHub username",
+                    name: 'engineerUsername'
+                }
+            ])
+            .then((response) => {
+                console.log('Engineer', response)
+                createTeam()
 
-    // function addIntern()
+            })
+    }
+
+    function addIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: "Enter the intern's name",
+                    name: 'internName'
+                },
+                {
+                    type: 'input', 
+                    message: "Enter the intern's ID", 
+                    name: 'internId'
+                },
+                {
+                    type: 'input',
+                    message: "Enter the intern's email",
+                    name: 'internEmail'
+                },
+                {
+                    type: 'input', 
+                    message: "Enter the intern's school",
+                    name: 'internSchool'
+                }
+            ])
+            .then((response) => {
+                console.log('Intern', response)
+                createTeam()
+            })
+    }
 
     // function buildTeam()
 
